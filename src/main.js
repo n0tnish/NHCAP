c465fd0cbabc2e908d7eff23247052b151c70a3a
n0tnish <nishaatgafieldien@gmail.com>
2024-03-17 22:04:26 +0200

    css + style.css added and code input

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-// Import Bootstrap CSS using CDN
+//  Bootstrap import CSS thing using CDN. yesis. uiteindelik
 const linkElement = document.createElement('link')
 linkElement.rel = 'stylesheet'
 linkElement.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'
