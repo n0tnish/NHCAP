160690ac0403a57625d474e61e3cc5262994f14b
n0tnish <nishaatgafieldien@gmail.com>
2024-03-18 13:54:37 +0200

    about, ig

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h2>Nurul Huda (نور الهدی) translated means "a light of guidance" or "A guiding light" and that's exactly what our business aims to be. Here at Nurul Huda, we understand that modesty is a part of faith </h2>
   </div>
 </template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>
+
+</style>
