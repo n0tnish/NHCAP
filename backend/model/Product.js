f1bf685deef84753b77a826806478c1473e02b14
n0tnish <nishaatgafieldien@gmail.com>
2024-03-18 16:07:01 +0200

    model, product,Users.js

---
+++ backend/model/Product.js
@@ -0,0 +1,79 @@
+import {
+    connection
+} from "../config/index.js"
+
+class Products {
+    fetchProducts(req, res) {
+        const dbQry = `
+        SELECT prodID, prodName, quantity,
+        amount, Category, prodURL, prodDesc
+        FROM Products;
+        `
+        connection.query(dbQry, (error, results) => {
+            if (error) throw error
+            res.json({
+                status: res.errorCode,
+                results
+            })
+        })
+    }
+    fetchProduct(req, res) {
+        const dbQry = `
+        SELECT prodID, prodName, quantity,
+        amount, Category, prodURL, prodDesc
+        FROM Products
+        WHERE prodID = ${req.params.id};
+        `
+        connection.query(dbQry, (error, result) => {
+            if (error) throw error
+            res.json({
+                status: res.errorCode,
+                result: result[0]
+            })
+        })
+    }
+    addProduct(req, res) {
+        const dbQry = `
+        INSERT INTO Products
+        SET ?;
+        `
+        connection.query(dbQry, [req.body], (error) => {
+            if (error) throw error
+            res.json({
+                status: res.errorCode,
+                message: 'New product added successfully'
+            })
+        })
+    }
+    updateProduct(req, res) {
+        const dbQry = `
+        UPDATE Products
+        SET ?
+        WHERE prodID = ${req.params.id};
+        `
+        connection.query(dbQry, [req.body], (error) => {
+            if (error) throw error
+            res.json({
+                status: res.errorCode,
+                message: "Product information successfully updated"
+            })
+        })
+    }
+    deleteProduct(req, res) {
+        const dbQry = `
+        DELETE FROM Products
+        WHERE prodID = ${req.params.id};
+        `
+        connection.query(dbQry, (error) => {
+            if (error) throw error
+            res.json({
+                status: res.statusCode,
+                message: "Product successfully deleted."
+            })
+        })
+        }
+}
+
+export {
+    Products
+}
